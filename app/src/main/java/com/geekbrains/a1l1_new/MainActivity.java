4efc9fabfd2cef3b3bbbb92151f833b5c0a94464
Alexander Voronov <dr.strum@gmail.com>
2020-08-06 15:19:12 +0300

    HW3_06/08

--- app/src/main/java/com/geekbrains/a1l1_new/MainActivity.java
+++ app/src/main/java/com/geekbrains/a1l1_new/MainActivity.java
@@ -3,12 +3,100 @@ package com.geekbrains.a1l1_new;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView counterTextView;
+    private Button randomCounterBtn;
+    private final String counterDataKey = "counterDataKey";
+    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        findViews();
+        setOnClickBehaviourToBtn();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        String text = counterTextView.getText().toString();
+        saveInstanceState.putString(counterDataKey, text);
+        super.onSaveInstanceState(saveInstanceState);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String text = savedInstanceState.getString(counterDataKey);
+        counterTextView.setText(text);
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    private void findViews() {
+        counterTextView = findViewById(R.id.containerTextView);
+        randomCounterBtn = findViewById(R.id.randomCounterBtn);
+    }
+
+    private void setOnClickBehaviourToBtn() {
+        randomCounterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int count = Integer.parseInt(counterTextView.getText().toString());
+                String newValue = String.valueOf(++count);
+                counterTextView.setText(newValue);
+
+            }
+        });
+    }
+
 }
